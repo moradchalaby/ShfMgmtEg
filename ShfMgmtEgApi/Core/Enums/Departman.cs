162ab98f089275df831917fde5121b0d6684f41b
Murat Çelebi <49117869+moradchalaby@users.noreply.github.com>
2024-03-29 10:34:02 +0300

    Update Enums

---
+++ ShfMgmtEgApi/Core/Enums/Departman.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ShfMgmtEgApi.Core.Enums;
+
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum Departman
+{
+    IT = 1,
+    HR = 2,
+    Technic = 3,
+}
